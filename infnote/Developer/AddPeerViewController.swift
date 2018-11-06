4bd212659eea1f0610cc1ff1f7aacc955c2a1c2d
Vergil Choi <gameboy0824@126.com>
2018-11-06 15:23:33 +0800

    Add default peer info

--- infnote/Developer/AddPeerViewController.swift
+++ infnote/Developer/AddPeerViewController.swift
@@ -22,13 +22,28 @@ class AddPeerViewController: UITableViewController {
     }
     
     @IBAction func addButtonTouched(_ sender: Any) {
-        guard let address = addressField.text,
-            let portString = portField.text,
-            let port = Int(portString),
-            let peer = Peer(address: address, port: port) else {
+        guard var address = addressField.text,
+            var portString = portField.text,
+            var rankString = rankField.text else {
             return
         }
-        PeerManager.shared.addOrUpdate(peer)
-        navigationController?.popViewController(animated: true)
+        
+        address = address.count <= 0 ? addressField.placeholder! : address
+        portString = portString.count <= 0 ? portField.placeholder! : portString
+        rankString = rankString.count <= 0 ? rankField.placeholder! : rankString
+
+        if let port = Int(portString),
+            let rank = Int(rankString),
+            address.count > 0,
+            port > 0 && port < 65536,
+            let peer = Peer(address: address, port: port, rank: rank) {
+            PeerManager.shared.addOrUpdate(peer)
+            navigationController?.popViewController(animated: true)
+            return
+        }
+        
+        let alert = UIAlertController(title: nil, message: "Invalid format of peer information", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true)
     }
 }
